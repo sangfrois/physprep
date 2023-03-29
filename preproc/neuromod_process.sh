5b503131925acd8788a7883158daeb459c6fee2f
Marie-Êve Picard <mepicard@elm.criugm.qc.ca>
2023-03-29 09:05:30 -0400

    update bash file

--- preproc/neuromod_process.sh
+++ preproc/neuromod_process.sh
@@ -3,25 +3,17 @@
 SOURCE="/scratch/mepicard/physio_data/emotionvideos"
 OUTDIR="/scratch/mepicard/physio_data/emotionvideos/processed"
 
-python neuromod_process.py $SOURCE "sub-02" "ses-001" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-02" "ses-002" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-02" "ses-003" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-02" "ses-004" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-02" "ses-005" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-02" "ses-006" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-02" "ses-007" $OUTDIR True
-
-python neuromod_process.py $SOURCE "sub-03" "ses-002" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-03" "ses-003" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-03" "ses-004" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-03" "ses-005" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-03" "ses-006" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-03" "ses-007" $OUTDIR True
-
-python neuromod_process.py $SOURCE "sub-05" "ses-001" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-05" "ses-002" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-05" "ses-003" $OUTDIR True
-python neuromod_process.py $SOURCE "sub-05" "ses-004" $OUTDIR True
-
-
-
+for dir in $SOURCE/*/
+do
+    #Iterate over sub folders
+    if [[ $dir == *"sub"* ]]
+    then
+        SUB=$(basename $dir)
+        #Iterate over ses folders
+        for i in $dir*/
+        do
+            SES=$(basename $i)
+            python neuromod_process.py $SOURCE $SUB $SES $OUTDIR True
+        done
+    fi
+done
