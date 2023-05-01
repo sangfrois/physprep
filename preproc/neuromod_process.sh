f0efbfc5f379b79dbe10b84ebdf7f277841cdfc8
Marie-Êve Picard <mepicard@elm.criugm.qc.ca>
2023-05-01 08:29:37 -0400

    change sh

--- preproc/neuromod_process.sh
+++ preproc/neuromod_process.sh
@@ -3,23 +3,25 @@
 SOURCE="/scratch/mepicard/physio_data/emotionvideos"
 OUTDIR="/scratch/mepicard/physio_data/emotionvideos/processed"
 
-for dir in $SOURCE/*/
-do
+python process.py $SOURCE sub-06 ses-002 $OUTDIR True
+
+#for dir in $SOURCE/*/
+#do
     #Iterate over sub folders
-    if [[ $dir == *"sub"* ]]
-    then
-        SUB=$(basename $dir)
+#    if [[ $dir == *"sub"* ]]
+#    then
+#        SUB=$(basename $dir)
         #Iterate over ses folders
-        for i in $dir*/
-        do
-            SES=$(basename $i)
+#        for i in $dir*/
+#        do
+#            SES=$(basename $i)
             # neuromod_process.py takes in argument :
             # 1. the path to source data
             # 2. the subject id (folder name)
             # 3. the ses id (folder name)
             # 4. the path to save the outputs
             # 5. Boolean to indicate wheter or not the outputs should be saved
-            python process.py $SOURCE $SUB $SES $OUTDIR True
-        done
-    fi
-done
+#            python process.py $SOURCE $SUB $SES $OUTDIR True
+#        done
+#    fi
+#done
